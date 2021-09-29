8539fb228d5e22b442a2720c5bba6ef944fcb364
BowenLi1994 <bli@cs.fsu.edu>
2021-09-29 17:51:08 -0400

    update string_label class

--- src/label/string_label.h
+++ src/label/string_label.h
@@ -1,24 +1,3 @@
-// The MIT License (MIT)
-// Copyright (c) 2017 Mateusz Pawlik, Nikolaus Augsten, and Daniel Kocher.
-//
-// Permission is hereby granted, free of charge, to any person obtaining a copy
-// of this software and associated documentation files (the "Software"), to deal
-// in the Software without restriction, including without limitation the rights
-// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-// copies of the Software, and to permit persons to whom the Software is
-// furnished to do so, subject to the following conditions:
-//
-// The above copyright notice and this permission notice shall be included in
-// all copies or substantial portions of the Software.
-//
-// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-// SOFTWARE.
-
 /// \file label/string_label.h
 ///
 /// \details
@@ -33,21 +12,8 @@
 
 namespace label {
 
-/// \class StringLabel
-///
-/// \details
-/// Represents the (satellite) data a node can hold. This class only serves as
-/// encapsulation of this data and all operations on this data, nothing else.
-///
-/// \remark
-/// A user of this framework may want to create a specialized data class to
-/// suffice his/her application area, e.g., holding a sequence of sequences of
-/// <needed-type> to encode their data properly. One needs at least to implement
-/// the following member functions such that it does not break the framework:
-///     - bool operator==(...) const; // aka the 'equals' operator.
-/// The member function to get the member are optional but may be necessary if
-/// one wants to use a specialized cost model.
 class StringLabel {
+  
 public:
     StringLabel(const std::string& label);
 
